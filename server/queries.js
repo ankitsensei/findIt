99f9fcb4e43e8ca0d269befb027af8ff65d86c32
ankitsensei <ankitbhagat.dev@gmail.com>
2026-07-25 22:17:14 +0530

    feat: getting all entries from getLostItems

--- server/queries.js
+++ server/queries.js
@@ -1,14 +1,22 @@
+import "dotenv/config";
 import pg from "pg";
 const { Pool } = pg;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+// Lost Items
 const getLostItems = async (req, res) => {
-  console.log("Get all lost items");
-  res.send("Get all lost items");
+  try {
+    const results = await pool.query("SELECT * FROM lostitems");
+    res.status(200).json(results.rows);
+    res.status(200).json({ message: "hey" });
+  } catch (error) {
+    throw error;
+  }
 };
 
+// Found Items
 const getFoundItems = async (req, res) => {
   console.log("Get all found items");
   res.send("Get all found items");
