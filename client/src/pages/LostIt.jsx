a171b0287cfe723b37c9cb22986dd252cc0c7700
ankitsensei <ankitbhagat.dev@gmail.com>
2026-07-27 09:05:09 +0530

    feat: fetching lostData

--- client/src/pages/LostIt.jsx
+++ client/src/pages/LostIt.jsx
@@ -1,6 +1,35 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const LostIt = () => {
+  const [lostData, setLostData] = useState([]);
+  const [lostDataLoaded, setLostDataLoaded] = useState(false);
+
+  const fetchLostData = async () => {
+    try {
+      const res = await fetch(`http://localhost:3000/lostItems`);
+      const json = await res.json();
+      setLostData(json);
+      setLostDataLoaded(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      fetchLostData();
+    }, 300);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  console.log(lostData);
+
+  if (!lostDataLoaded)
+    return (
+      <div className="h-screen w-full flex items-center justify-center">
+        <p>Loading...</p>
+      </div>
+    );
   return <div>LostIt</div>;
 };
 
