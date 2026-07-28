24cebcf2b23a87b6c9689c867ee8523e0c8c8612
ankitsensei <ankitbhagat.dev@gmail.com>
2026-07-28 21:57:53 +0530

    feat: pagination limit 20

--- server/queries.js
+++ server/queries.js
@@ -29,7 +29,7 @@ const uploadImage = (buffer, folder) => {
 const getLostItems = async (req, res) => {
   try {
     const results = await pool.query(
-      "SELECT * FROM lostitems ORDER BY created_at DESC",
+      "SELECT * FROM lostitems ORDER BY created_at DESC LIMIT 20 OFFSET 0",
     );
     res.status(200).json(results.rows);
   } catch (error) {
