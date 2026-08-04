723f520bebd0616ffd5c58363799d4b2fd0ed986
ankitsensei <ankitbhagat.dev@gmail.com>
2026-08-04 21:17:09 +0530

    feat: getting user data from backend

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const Profile = () => {
+  const token = localStorage.getItem("token");
+  const [useData, setUserData] = useState([]);
+  const handleUserProfile = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/me`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const user = await response.json();
+      // console.log(user);
+      setUserData(user);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    handleUserProfile();
+    console.log(useData);
+  }, []);
   return (
     <div className="min-h-screen bg-zinc-50 pt-22 md:pt-26 pb-8 md:pb-12 px-4 md:px-6">
       Profile
