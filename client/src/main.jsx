4d9d0e9294756aaedf6f1323302c0b43a88d2d8b
ankitsensei <ankitbhagat.dev@gmail.com>
2026-08-04 18:21:42 +0530

    feat: route se for user profile page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,8 @@ import LostIt from "./pages/LostIt.jsx";
 import FoundIt from "./pages/FoundIt.jsx";
 import ViewDetails from "./components/ViewDetails.jsx";
 import Signup from "./pages/Signup.jsx";
-import Signin from "./pages/Signin.jsx";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -23,6 +24,7 @@ createRoot(document.getElementById("root")).render(
         <Route path="/foundit/:id" element={<ViewDetails />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="min-h-screen bg-zinc-50 pt-22 md:pt-26 pb-8 md:pb-12 px-4 md:px-6">
+      Profile
+    </div>
+  );
+};
+
+export default Profile;
